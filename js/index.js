b2bbc7dde8c0af02fd50c1d85bdb0922db195896
Franck VALLY <franck.vally@laposte.net>
2020-07-17 17:11:54 +0200

    3

--- js/index.js
+++ js/index.js
@@ -1,19 +1,7 @@
 // URL de l'api
 const url = "http://localhost:3000/api/cameras";
 
-// Affiche les produits
-const displayProducts = async () => {
-  const products = await getAllCams(url);
-  products.forEach((product) => {
-    renderProduct(product.name, product._id, product.imageUrl, product.price);
-  });
-};
 
-// Récupère toutes les caméras
-const getAllCams = async (url) => {
-  const response = await fetch(url);
-  return await response.json();
-};
 
 // Faire l'affichage d'un produit
 function renderProduct(productName, productId, productImg, productPrice) {
