ec180923dfdbc13b27213595a60d85b4da93faf8
Franck VALLY <franck.vally@laposte.net>
2020-07-17 16:26:31 +0200

    premier

---
+++ README.md
@@ -0,0 +1,24 @@
+# Orinoco #
+
+This is the back end server for Project 5 of the Junior Web Developer path.
+
+### Prerequisites ###
+
+You will need to have Node and `npm` installed locally on your machine.
+
+### Installation ###
+
+Clone this repo. From within the project folder, run `npm install`. You 
+can then run the server with `node server`. 
+The server should run on `localhost` with default port `3000`. If the
+server runs on another port for any reason, this is printed to the
+console when the server starts, e.g. `Listening on port 3001`.
+
+
+Clonez ce dépôt. Depuis le dossier du projet, exécutez `npm install`.
+Vous pouvez alors exécuter le serveur avec `node server`
+Le serveur doit fonctionner sur `localhost` avec le port par défaut `3000`. Si le
+serveur fonctionne sur un autre port pour une raison quelconque, il est imprimé sur le
+console au démarrage du serveur, par ex. `Listening on port 3001`.
+
+Si votre navigateur ne démarre pas : ouvrez index.html dans celui-ci.
---
+++ js/index.js
@@ -0,0 +1,31 @@
+// URL de l'api
+const url = "http://localhost:3000/api/cameras";
+
+// Affiche les produits
+const displayProducts = async () => {
+  const products = await getAllCams(url);
+  products.forEach((product) => {
+    renderProduct(product.name, product._id, product.imageUrl, product.price);
+  });
+};
+
+// Récupère toutes les caméras
+const getAllCams = async (url) => {
+  const response = await fetch(url);
+  return await response.json();
+};
+
+// Fourni l'affichage d'un produit
+function renderProduct(productName, productId, productImg, productPrice) {
+  const products = document.querySelector("#produits");    // Récupère la div qui contiendra les différents articles
+  const article = document.createElement("article");       //  construction de l'élément contenant les carractéristique article
+  article.innerHTML = 
+    `<a href="produits.html?id=${productId}" style="display:block;width:100%;height:100%;">
+    <img alt="${productName}" src="${productImg}">
+    <p class="produits-title">${productName}</p>
+    <p class="price">${productPrice / 100}</p>
+    </a>`;
+  products.appendChild(article);
+}
+
+displayProducts();
